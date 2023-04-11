1297a165809b6da0dfe297237a2e78788f6d5fb2
Mikaella Muñoz <mikamub@gmail.com>
2023-04-11 15:20:30 -0400

    JS interaccion libros, info bien pero desaparecen los libros

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ document.getElementById("botonInicioHP").addEventListener('click', function () {
 // Funcionalidad para el boton de libros
 document.getElementById("botonInicioLibros").addEventListener('click', function () { //aqui se llama el id del boton 
   window.scrollTo({ // atributo de mover por la pagina
-    top: document.getElementById("paginaLibros").offsetTop, //aqui se le indica que tiene que moverse verticalmete hasta el id-div tambien puede ser query.selector
+    top: document.getElementById("libros").offsetTop, //aqui se le indica que tiene que moverse verticalmete hasta el id-div tambien puede ser query.selector
     behavior: "smooth" //forma en que se mueve, en este caso es lento tambien esta instantaneo "instant"
   });
 });
@@ -48,6 +48,19 @@ document.getElementById("botonInicioPociones").addEventListener('click', functio
   });
 });
 
+const infoLibros = document.getElementById("libros");
+infoLibros.addEventListener('click', function(event){
+  if(event.target.classList.contains("LibroPortada")){
+    const valor = event.target.getAttribute('value');
+    const id = event.target.id;
+    let titulo = data.books[valor].title;
+    infoLibros.innerText = titulo;
+    console.log("Valor: ", valor);
+    console.log("ID: ", id);
+  }
+  
+}
+);
 // primerLibroTooltip.addEventListener('click', function () {
 //   const A = document.getElementById('primerLibroTooltip');
 //   let i = primerLibroTooltip.getAttribute('value')
