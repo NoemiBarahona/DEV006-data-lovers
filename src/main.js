e375a993e524d80eef2fd7d48a54db61f95a1293
Noemipurr <noebeatrizm@gmail.com>
2023-04-11 12:51:53 -0300

    arreglo de las imagenes para automatizar

--- src/main.js
+++ src/main.js
@@ -5,21 +5,21 @@ import data from './data/harrypotter/data.js';
 console.log(data.books[0]);
 
 // Funcionalidad para el boton de inicio
-document.getElementById("botonInicioHP").addEventListener('click', function() { //aqui se llama el id del boton 
+document.getElementById("botonInicioHP").addEventListener('click', function () { //aqui se llama el id del boton 
   window.scrollTo({ // atributo de mover por la pagina
     top: 0, //aqui se le indica que tiene que moverse verticalmete hasta el punto 0/inicio
     behavior: "smooth"//forma en que se mueve, en este caso es lento tambien esta instantaneo "instant"
   });
-});  
+});
 // Funcionalidad para el boton de libros
-document.getElementById("botonInicioLibros").addEventListener('click', function() { //aqui se llama el id del boton 
+document.getElementById("botonInicioLibros").addEventListener('click', function () { //aqui se llama el id del boton 
   window.scrollTo({ // atributo de mover por la pagina
     top: document.getElementById("paginaLibros").offsetTop, //aqui se le indica que tiene que moverse verticalmete hasta el id-div tambien puede ser query.selector
     behavior: "smooth" //forma en que se mueve, en este caso es lento tambien esta instantaneo "instant"
   });
-});  
+});
 // Funcionalidad para el boton de casas
-document.getElementById("botonInicioCasas").addEventListener('click', function() { //aqui se llama el id del boton 
+document.getElementById("botonInicioCasas").addEventListener('click', function () { //aqui se llama el id del boton 
   window.scrollTo({ // atributo de mover por la pagina
     top: document.getElementById("PORDEFINIR").offsetTop, //aqui se le indica que tiene que moverse verticalmete hasta el id-div tambien puede ser query.selector
     behavior: "smooth" //forma en que se mueve, en este caso es lento tambien esta instantaneo "instant"
@@ -27,35 +27,35 @@ document.getElementById("botonInicioCasas").addEventListener('click', function()
 });
 
 // Funcionalidad para el boton de personajes
-document.getElementById("botonInicioPersonajes").addEventListener('click', function() {//aqui se llama el id del boton 
+document.getElementById("botonInicioPersonajes").addEventListener('click', function () {//aqui se llama el id del boton 
   window.scrollTo({// atributo de mover por la pagina
     top: document.getElementById("PORDEFINIR").offsetTop, //aqui se le indica que tiene que moverse verticalmete hasta el id-div tambien puede ser query.selector
     behavior: "smooth" //forma en que se mueve, en este caso es lento tambien esta instantaneo "instant"
   });
-});  
+});
 // Funcionalidad para el boton de hechizos
-document.getElementById("botonInicioHechizos").addEventListener('click', function() {//aqui se llama el id del boton 
+document.getElementById("botonInicioHechizos").addEventListener('click', function () {//aqui se llama el id del boton 
   window.scrollTo({// atributo de mover por la pagina
     top: document.getElementById("PORDEFINIR").offsetTop,
     behavior: "smooth"
   });
-});  
+});
 // Funcionalidad para el boton de pociones
-document.getElementById("botonInicioPociones").addEventListener('click', function() {//aqui se llama el id del boton 
+document.getElementById("botonInicioPociones").addEventListener('click', function () {//aqui se llama el id del boton 
   window.scrollTo({// atributo de mover por la pagina
     top: document.getElementById("PORDEFINIR").offsetTop, //aqui se le indica que tiene que moverse verticalmete hasta el id-div tambien puede ser query.selector
     behavior: "smooth" //forma en que se mueve, en este caso es lento tambien esta instantaneo "instant"
   });
-});  
-
-primerLibroTooltip.addEventListener('click', function() {
-  const A = document.getElementById('primerLibroTooltip');
-  let i = primerLibroTooltip.getAttribute('value')
-  console.log(parseInt(i))
-  let titulo = data.books[parseInt(i)].title;
-   // Ejemplo de cómo mostrar los datos en el tooltip
-  primerLibroTooltip.innerText = titulo;
 });
+
+// primerLibroTooltip.addEventListener('click', function () {
+//   const A = document.getElementById('primerLibroTooltip');
+//   let i = primerLibroTooltip.getAttribute('value')
+//   console.log(parseInt(i))
+//   let titulo = data.books[parseInt(i)].title;
+//   // Ejemplo de cómo mostrar los datos en el tooltip
+//   primerLibroTooltip.innerText = titulo;
+// });
 // primerLibroTooltip.addEventListener('click', function() {
 //   const tooltipData = data.books[0].releaseDay; // Ejemplo de cómo mostrar los datos en el tooltip
 //   primerLibroTooltip.innerText = tooltipData;
