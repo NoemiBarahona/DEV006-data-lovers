89f4090582461008e4d6d8e9ef7f8110ad973b4c
Noemipurr <noebeatrizm@gmail.com>
2023-04-13 12:13:00 -0300

    Manipulacion de data arreglada sin diseño responsivo

--- src/main.js
+++ src/main.js
@@ -48,24 +48,22 @@ document.getElementById("botonInicioPociones").addEventListener('click', functio
   });
 });
 
-const infoLibros = document.getElementById("libros"); // Constante que llama al id de libro deol HTML
-const infoDataLibro = document.createElement("p"); // Crear una costante que crea un parrafo para la data libro
-infoDataLibro.classList.add("info-libro"); // Para darle el estilo al parrafo que se esta creando para mostar la data detras de la imagen
-infoLibros.addEventListener('mouseover', function (event) { // Evento para que cuando el mouse pase por la imagen muestre la data
-  if (event.target.classList.contains("libroPortada")) { //Es para que todos los libros que tienen la class libroPortada tengan el event de arriba
-    const valor = event.target.getAttribute('value'); // Costante para obtener el atributo de value de los libros
-    const id = event.target.id; // Constante llamada ID para que llame al id de la imagen seleccionada
-    const titulo = data.books[valor].title; // Contante titulo que llama el value de la data de libro 
-    const fechaLanzamiento = data.books[valor].releaseDay; // Contante fechaLanzamiento que llama el value de la data de libro 
-    const libro = document.getElementById(id); // Constante libro que llama a la contante id creada en 3 linea arriba
-    infoDataLibro.innerText = titulo + " " + fechaLanzamiento; // Info data libro que muestre el titulo más la fecha en la pag
-    libro.appendChild(infoDataLibro); //  Donde esta ubicado el libro se agrege la info de data libro
-  }
-});
-infoLibros.addEventListener('mouseout', function (event) { //Evento que al quitar el mouse de la imagen desapasca el texto mostrando solo la imagen
-  if (event.target.classList.contains("libroPortada")) { // Evento muestra el contenido de libro portada
-    const id = event.target.id; // LLama el ID de libro
-    const libro = document.getElementById(id); // LLama el ID de libro
-    libro.removeChild(infoDataLibro); // Que se remueva el parrafo creado 
-  }
+const infoDataLibro = document.createElement("p");// Crear una costante que crea un parrafo para la data libro
+infoDataLibro.classList.add("info-libro");// Para darle una clase de estilo llamada info-libroal parrafo que se esta creando para mostar la data detras de la imagen
+
+document.querySelectorAll(".libroPortada").forEach(function(libro) { //para seleccionar todos los elementos en el documento con la clase "libroPortada" y agregarles dos event listeners.
+  libro.addEventListener('mouseover', function () { //Event listener se dispara cuando el cursor se coloca sobre la imagen del libro y el segundo cuando se mueve el cursor fuera de la imagen.
+    const valor = libro.getAttribute('value'); // Constante para obtener el atributo de value de los libros
+    const titulo = data.books[valor].title;// Constante titulo que llama el value de la data de libro 
+    const fechaLanzamiento = data.books[valor].releaseDay;// Constante fechaLanzamiento que llama el value de la data de libro 
+    const autor = data.books[valor].author; // Constante autor que llama el value de la data de libro 
+    const descripcion = data.books[valor].description;// Constante descripction que llama el value de la data de libro 
+
+    infoDataLibro.innerText = "Libro: " + titulo + "\n" + "Día de lanzamiento: " + fechaLanzamiento + "\n" + "Autor: " + autor + "\n" + "Descripción: " + descripcion;// Info data libro que muestre el titulo más la fecha en la pag
+    libro.appendChild(infoDataLibro);// Donde esta ubicado el libro se agrege la info de data libro
+  });
+
+  libro.addEventListener('mouseleave', function () { // Esto muestra la información del libro debajo de su imagen.
+    libro.removeChild(infoDataLibro); //para quitar el párrafo que muestra la información del libro de debajo de la imagen.
+  });
 });
