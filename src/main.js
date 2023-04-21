b3e11c2e07112cced8cb733a5bf5c5a8d4f66e3e
Noemipurr <noebeatrizm@gmail.com>
2023-04-21 11:11:31 -0300

    Paginas hechas sin linkear

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
-import { filterCharacterGender,filterCharacterName, filterCharacterHouse, sortData } from './data.js';
+// import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/harrypotter/data.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 // console.log(data.books[0]);
-console.log(filterCharacterName(data.characters,'Black'))
-console.log(filterCharacterGender(data.characters,'Female'))
-const dataOrdenada = sortData(data.characters, "name")
-console.log(filterCharacterGender(dataOrdenada,'Female'))
-console.log(filterCharacterHouse(data.characters, 'Slytherin'))
-
 // Funcionalidad para el boton de inicio
 document.getElementById("botonInicioHP").addEventListener('click', function () { //aqui se llama el id del boton
   window.scrollTo({ // atributo de mover por la pagina
@@ -16,7 +10,6 @@ document.getElementById("botonInicioHP").addEventListener('click', function () {
     behavior: "smooth"//forma en que se mueve, en este caso es lento tambien esta instantaneo "instant"
   });
 });
-
 // Funcionalidad para el boton de libros
 document.getElementById("botonInicioLibros").addEventListener('click', function () { //aqui se llama el id del boton
   window.scrollTo({ // atributo de mover por la pagina
@@ -24,7 +17,6 @@ document.getElementById("botonInicioLibros").addEventListener('click', function 
     behavior: "smooth" //forma en que se mueve, en este caso es lento tambien esta instantaneo "instant"
   });
 });
-
 // Funcionalidad para el boton de personajes
 document.getElementById("botonInicioPersonajes").addEventListener('click', function () {//aqui se llama el id del boton
   window.scrollTo({// atributo de mover por la pagina
@@ -46,26 +38,20 @@ document.getElementById("botonInicioPociones").addEventListener('click', functio
     behavior: "smooth" //forma en que se mueve, en este caso es lento tambien esta instantaneo "instant"
   });
 });
-
 const botonHamburguesa = document.getElementById("botonHamburguesa");
 const contenedorBoton = document.getElementById("navbar");
 // const flexbox = document.querySelector(".flexbox");
-
 botonHamburguesa.addEventListener("click", () => {
-  
   // flexbox.classList.toggle("oculto");
-  if(contenedorBoton.classList.contains("navbar-open")){
+  if (contenedorBoton.classList.contains("navbar-open")) {
     contenedorBoton.removeAttribute('class', "navbar-open")
     contenedorBoton.classList.toggle("navbar-close");
-    
-  }else{
+  } else {
     contenedorBoton.classList.toggle("navbar-open");
   }
 });
-
 const infoDataLibro = document.createElement("p");
 infoDataLibro.classList.add("info-libro");
-
 document.querySelectorAll(".libroPortada").forEach(function (libro) {
   libro.addEventListener('mouseover', function () {
     const valor = libro.getAttribute('value');
@@ -73,36 +59,27 @@ document.querySelectorAll(".libroPortada").forEach(function (libro) {
     const fechaLanzamiento = data.books[valor].releaseDay;
     const autor = data.books[valor].author;
     const descripcion = data.books[valor].description;
-
     infoDataLibro.innerText = "Book: " + titulo + "\n" + "Release Day: " + fechaLanzamiento + "\n" + "Author: " + autor + "\n" + "Description: " + descripcion;
     libro.appendChild(infoDataLibro);
-
     // Agregar la clase 'girar' a la imagen
     libro.querySelector('img').classList.add('rotar');
   });
-
   libro.addEventListener('mouseleave', function () {
     // Eliminar el párrafo de información del libro
     libro.removeChild(infoDataLibro);
-
     // Esperar a que se complete la transición y luego agregar la clase 'mostrar-info'
     libro.querySelector('img').addEventListener('transitionend', function () {
       libro.querySelector('img').classList.remove('rotar');
       libro.classList.remove('mostrar-info');
     }, { once: true });
-
     // Agregar la clase 'mostrar-info' a la capa contenedora del libro
     libro.classList.add('mostrar-info');
   });
 });
-
 const infoDataPersonaje = document.createElement("p");
 infoDataPersonaje.classList.add("info-personaje");
-
 document.querySelectorAll(".personaje").forEach(function (personaje) {
-
   personaje.addEventListener('mouseover', function () {
-
     const nombreId = personaje.getAttribute('id')
     const personajeData = data.characters.find(personaje => personaje.name === nombreId);
     let nombre;
@@ -112,14 +89,12 @@ document.querySelectorAll(".personaje").forEach(function (personaje) {
     let patronusPersonaje;
     let casa;
     let ancestros;
-
     if (personajeData) {
       nombre = personajeData.name;
       fechaNacimiento = personajeData.birth;
       if (fechaNacimiento === null) {
         fechaNacimiento = "Desconocida";
       }
-
       muerte = personajeData.death;
       if (muerte === null) {
         muerte = "Alive";
@@ -143,41 +118,34 @@ document.querySelectorAll(".personaje").forEach(function (personaje) {
       if (ancestros === null) {
         ancestros = "Desconocida";
       }
-
       infoDataPersonaje.innerText = "Name: " + nombre + "\n" + "Birth: " + fechaNacimiento + "\n" + "Status: " + muerte + "\n" + "Gender: " + genero + "\n" + "Patronus: " + patronusPersonaje + "\n" + "House: " + casa + "\n" + "Ancestry: " + ancestros;
       personaje.appendChild(infoDataPersonaje);
     };
     // Agregar la clase 'girar' a la imagen
     //personaje.querySelector('img').classList.add('rotar');
   });
-
   personaje.addEventListener('mouseleave', function () {
     // Eliminar el párrafo de información del libro
     personaje.removeChild(infoDataPersonaje);
-
     // Esperar a que se complete la transición y luego agregar la clase 'mostrar-info'
     personaje.querySelector('img').addEventListener('transitionend', function () {
       personaje.querySelector('img').classList.remove('rotar');
       personaje.classList.remove('mostrar-info');
     }, { once: true });
-
     // Agregar la clase 'mostrar-info' a la capa contenedora del libro
     personaje.classList.add('mostrar-info');
   });
 });
-
 const flechas = document.querySelectorAll('.flecha');
 flechas.forEach(flecha => {
   flecha.addEventListener('click', () => {
     // Obtener el contenedor de filtros dentro de la sección
     const seccion = flecha.parentNode;
     const filtros = seccion.querySelector('.filtro');
-    
     // Alternar la clase 'desplegado' para mostrar/ocultar los filtros
     filtros.classList.toggle('desplegado');
-    
     // Cambiar el texto de la flecha para indicar el estado de la sección (desplegada o no)
-    flecha.textContent = filtros.classList.contains('desplegado') ? '▼' : '▶️';
+    flecha.textContent = filtros.classList.contains('desplegado') ? '▼' : ':flecha_hacia_delante:';
   });
 });
 // const inputBusqueda = document.getElementById('input-busqueda');
@@ -192,8 +160,6 @@ flechas.forEach(flecha => {
 // el usuario pueda escribir su consulta de búsqueda. Luego, creamos un botón con el ícono de búsqueda usando
 //  la biblioteca de iconos Font Awesome. Al hacer clic en el botón, capturamos el valor de búsqueda del input
 //  usando JavaScript y podemos hacer algo con él, como enviarlo a una API o filtrar resultados en una lista.
-
-
 
 
 
